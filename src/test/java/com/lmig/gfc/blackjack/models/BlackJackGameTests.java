6e534117256a1a2d12253bfee481ace43527c67b
Gary Divine <gary.divine@libertymutual.com>
2017-12-01 16:43:32 -0500

    added unit tests for game status boolean methods

--- src/test/java/com/lmig/gfc/blackjack/models/BlackJackGameTests.java
+++ src/test/java/com/lmig/gfc/blackjack/models/BlackJackGameTests.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class BlackJackGameTests {
 	
 	@Test
-	public void playerWinsHandNoBlackjack_dealer_wins_with_blackjack() {
+	public void dealer_wins_with_blackjack() {
 		// Arrange
 		BlackJackGame bjg = new BlackJackGame();
 		Card dealerCard1 = new FaceCard(Suits.DIAMONDS, Faces.KING);
@@ -23,10 +23,17 @@ public class BlackJackGameTests {
 
       // Assert
 		assertThat(bjg.playerWinsHandNoBlackjack()).isEqualTo(false);
+		assertThat(bjg.playerWinsHandWithBlackjack()).isEqualTo(false);
+		assertThat(bjg.bothHaveBlackjacks()).isEqualTo(false);
+		assertThat(bjg.standardDraw()).isEqualTo(false);
+		assertThat(bjg.dealerWinsHand()).isEqualTo(true);
+		
+		
+		
 	}
 	
 	@Test
-	public void playerWinsHandNoBlackjack_dealer_wins_without_blackjack() {
+	public void dealer_wins_without_blackjack() {
 		// Arrange
 		BlackJackGame bjg = new BlackJackGame();
 		Card dealerCard1 = new FaceCard(Suits.DIAMONDS, Faces.KING);
@@ -42,10 +49,14 @@ public class BlackJackGameTests {
 
       // Assert
 		assertThat(bjg.playerWinsHandNoBlackjack()).isEqualTo(false);
+		assertThat(bjg.playerWinsHandWithBlackjack()).isEqualTo(false);
+		assertThat(bjg.bothHaveBlackjacks()).isEqualTo(false);
+		assertThat(bjg.standardDraw()).isEqualTo(false);
+		assertThat(bjg.dealerWinsHand()).isEqualTo(true);
 	}
 	
 	@Test
-	public void playerWinsHandNoBlackjack_player_wins_with_blackjack() {
+	public void player_wins_with_blackjack() {
 		// Arrange
 		BlackJackGame bjg = new BlackJackGame();
 		Card dealerCard1 = new FaceCard(Suits.DIAMONDS, Faces.KING);
@@ -61,10 +72,14 @@ public class BlackJackGameTests {
 
       // Assert
 		assertThat(bjg.playerWinsHandNoBlackjack()).isEqualTo(false);
+		assertThat(bjg.playerWinsHandWithBlackjack()).isEqualTo(true);
+		assertThat(bjg.bothHaveBlackjacks()).isEqualTo(false);
+		assertThat(bjg.standardDraw()).isEqualTo(false);
+		assertThat(bjg.dealerWinsHand()).isEqualTo(false);
 	}
 	
 	@Test
-	public void playerWinsHandNoBlackjack_player_wins_without_blackjack() {
+	public void player_wins_without_blackjack() {
 		// Arrange
 		BlackJackGame bjg = new BlackJackGame();
 		Card dealerCard1 = new FaceCard(Suits.DIAMONDS, Faces.KING);
@@ -82,6 +97,58 @@ public class BlackJackGameTests {
 
       // Assert
 		assertThat(bjg.playerWinsHandNoBlackjack()).isEqualTo(true);
+		assertThat(bjg.playerWinsHandWithBlackjack()).isEqualTo(false);
+		assertThat(bjg.bothHaveBlackjacks()).isEqualTo(false);
+		assertThat(bjg.standardDraw()).isEqualTo(false);
+		assertThat(bjg.dealerWinsHand()).isEqualTo(false);
+	}
+	
+	@Test
+	public void draw_without_blackjacks() {
+		// Arrange
+		BlackJackGame bjg = new BlackJackGame();
+		Card dealerCard1 = new FaceCard(Suits.DIAMONDS, Faces.KING);
+		Card dealerCard2 = new NumberCard(Suits.DIAMONDS, 3);
+		Card dealerCard3 = new NumberCard(Suits.DIAMONDS, 7);
+		Card playerCard1 = new FaceCard(Suits.CLUBS, Faces.KING);
+		Card playerCard2 = new FaceCard(Suits.DIAMONDS, Faces.KING);
+		bjg.getDealerHand().getHandArray().add(dealerCard1);
+		bjg.getDealerHand().getHandArray().add(dealerCard2);
+		bjg.getDealerHand().getHandArray().add(dealerCard3);
+		bjg.getPlayerHand().getHandArray().add(playerCard1);
+		bjg.getPlayerHand().getHandArray().add(playerCard2);
+
+      // Act
+
+      // Assert
+		assertThat(bjg.playerWinsHandNoBlackjack()).isEqualTo(false);
+		assertThat(bjg.playerWinsHandWithBlackjack()).isEqualTo(false);
+		assertThat(bjg.bothHaveBlackjacks()).isEqualTo(false);
+		assertThat(bjg.standardDraw()).isEqualTo(true);
+		assertThat(bjg.dealerWinsHand()).isEqualTo(false);
+	}
+	
+	@Test
+	public void draw_with_blackjacks() {
+		// Arrange
+		BlackJackGame bjg = new BlackJackGame();
+		Card dealerCard1 = new FaceCard(Suits.DIAMONDS, Faces.KING);
+		Card dealerCard2 = new AceCard(Suits.SPADES);
+		Card playerCard1 = new AceCard(Suits.CLUBS);
+		Card playerCard2 = new FaceCard(Suits.DIAMONDS, Faces.JACK);
+		bjg.getDealerHand().getHandArray().add(dealerCard1);
+		bjg.getDealerHand().getHandArray().add(dealerCard2);
+		bjg.getPlayerHand().getHandArray().add(playerCard1);
+		bjg.getPlayerHand().getHandArray().add(playerCard2);
+
+      // Act
+
+      // Assert
+		assertThat(bjg.playerWinsHandNoBlackjack()).isEqualTo(false);
+		assertThat(bjg.playerWinsHandWithBlackjack()).isEqualTo(false);
+		assertThat(bjg.bothHaveBlackjacks()).isEqualTo(true);
+		assertThat(bjg.standardDraw()).isEqualTo(false);
+		assertThat(bjg.dealerWinsHand()).isEqualTo(false);
 	}
 
 
